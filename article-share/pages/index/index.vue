866109caa1cda94e0682b607eeb6d691d93376e5
qmysg <1799874512@qq.com>
2022-10-29 10:39:04 +0800

    init

---
+++ article-share/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view class="container">
+			<text>left</text>
+			<text>content</text>
+			<text>right</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped lang="scss">
+	.container {
+		@include flex()
+		color:$base-color;
+	}
+</style>
